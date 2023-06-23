dd62a64e85fdb7a8af8e7639926e58c73c2279ab
ramya5544 <ramyavishvhin1995@gmail.com>
2023-06-24 00:06:54 +0530

    reactshop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import Header from "./Header";
+import Footer from "./footer";
+import Section from "./Section";
+import Navbar from "./Navbar";
+function App(props) {
+    const { notes } = props;
+return (
+        <div className="App">
+            <Navbar/>
+            <Header />
+            <Section notes={notes}/>
+            <Footer />
+        </div>
+    )
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+const notes = [
+  {
+    id: 1,
+    name: 'Fancy Product',
+    price1: '$40.00 - ',
+    price2: '$80.00',
+    star: '',
+    linkname: 'View options',
+    important: true
+  },
+  {
+    id: 2,
+    name: 'Special Item',
+    price1: '$20.00 ',
+    price2: '$18.00',
+    star: '⭐',
+    linkname: 'Add to cart',
+    important: true
+  },
+  {
+    id: 3,
+    name: 'Sale Item',
+    price1: '$50.00 - ',
+    price2: '$25.00',
+    star: '',
+    linkname: 'Add to cart',
+    important: true
+  },
+  {
+    id: 4,
+    name: 'Popular Item',
+    price1: '',
+    price2: '$40.00',
+    star: '⭐',
+    linkname: 'Add to cart',
+    important: true
+  },
+  {
+    id: 5,
+    name: 'Sale Item',
+    price1: '$50.00 - ',
+    price2: '$25.00',
+    star: '',
+    linkname: 'Add to cart',
+    important: true
+  },
+  {
+    id: 6,
+    name: 'Fancy Product',
+    price1: '$120.00 -',
+    price2: '$280.00',
+    star: '',
+    linkname: 'View options',
+    important: true
+  },
+  {
+    id: 7,
+    name: 'Special Item',
+    price1: '$20.00 ',
+    price2: '$18.00',
+    star: '⭐',
+    linkname: 'Add to cart',
+    important: true
+  },
+  {
+    id: 8,
+    name: 'Popular Item',
+    price1: '',
+    price2: '$40.00',
+    star: '⭐',
+    linkname: 'Add to cart',
+    important: true
+  },
+];
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
